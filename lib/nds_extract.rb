eda77a75e65d2016735171d611d7cfc2f63947e3
burbage stacey <rstacey06@gmail.com>
2019-10-24 21:50:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,7 +37,19 @@ return  names
 end
 
 def total_gross(source)
- 
+ dir_to_earnings_hash = directors_totals(source)
+  dir_names = list_of_directors(source)
+  i = 0
+
+  total = 0
+
+  while i < dir_names.length do
+    dir_name = dir_names[i]
+    total += dir_to_earnings_hash[dir_name]
+    i += 1
+  end
+
+return  total
 end
 
 
